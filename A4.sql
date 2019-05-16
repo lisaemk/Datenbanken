7122bc7726f4839a9519de7c5b6a683ca5371ae9
Jene Kabro <jene@mail.de>
2019-05-16 15:05:24 +0200

    correction in lesson

--- A4.sql
+++ A4.sql
@@ -18,13 +18,13 @@
 -- 1. DONE What is the total cost for advertising per exhibition in newspapers?
     SELECT  SUM(cost)
     FROM    Advertisements
-    WHERE   mediumName = 'newspaper';
+    WHERE   mediumName = name AND type = 'newspaper'
+    GROUP BY exhibitionTitle;
 
 -- 2. DONE Which artifacts (their id) have never been part of an exhibition?
     SELECT  id
     FROM    Artifacts
-    WHERE   id NOT IN (SELECT  artifactId
-                            FROM    ExhibitedAt);
+    WHERE   id NOT IN (SELECT  artifactId FROM ExhibitedAt);
 
 -- 3. DONE Increase by 10% the value of sculptures with color either ‘white’ or ‘green’.
     UPDATE  Artifacts
@@ -34,19 +34,25 @@
                         WHERE color = 'white' OR color = 'green');
 
 -- 4. Which percentage of paintings are currently in some exhibition?
-    SELECT  id
-    FROM    ArtifactPaintings
+-- COUNT(*) zählt alle Reihen in der Tabelle
+    SELECT  100*COUNT(*)/SELECT COUNT(*) FROM ArtifactPaintings)
+    FROM    ArtifactPaintings P, ExhibitedAt A, Exhibitions E
+    WHERE P.id = A.artifactId AND A.exhibitionTitle = E.title AND E.startDate <= CURDATE() AND E.endDate >= CURDATE();
 
 
 -- 5. Display artists (their id) that have been influenced by at most 2 other artists; 
 -- display the number of influences. Remember to include artists with no influences.
-    SELECT  influenceeId, COUNT(influenceeId) as freq
-    FROM    InfluencedBy
-    GROUP BY influenceeId
-    WHERE   freq < 3;
+    SELECT  influenceeId, COUNT(influencerId)
+    FROM    Artists LEFT JOIN InfluencedBy ON id = influenceeId
+    GROUP BY id
+    HAVING COUNT(influencerId) <= 2;
 
 -- 6. Move upcoming exhibitions that include paintings from ‘Vincent Van Gogh’ to room ‘03-500’. 
-    SELECT  
+    UPDATE Exhibitions
+    SET room = '03-500'
+    WHERE EXISTS (SELECT * FROM ExhibitedAt, ArtifactPaintings, Artists, Artifacts
+        WHERE ExhibitedAt.artifactId = Artifacts.id AND Artifacts.id = ArtifactPaintings.id AND Artifacts.artistId = Artists.id
+        AND Artists.name = 'Vincent Van Gogh' AND ExhibitedAt.exhibitionTitle = Exhibitions.title AND Exhibitions. startDate > CURDATE());  
 
 -- 7. DONE Which sculptures (their ids) were part of some exhibition in 2018?
     SELECT  id
